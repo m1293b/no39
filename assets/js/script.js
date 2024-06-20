6295bfc87470b4ed7ce8fa7f300da5934f502dad
Máté Balla <m1293b@gmail.com>
2024-06-20 06:59:57 +0000

    the base of the idea is ready, now I can build on it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
 let rooms = [
     cosy = {
-        numbers: [],
+        numbers: [1,2,3,4],
+        features: ['King bed', 'Walk-in shower', ],
         description: '',
         priceFrom: '',
-        
+    },
+    excellent = {
+        numbers: [5,6,7,8],
+        features: [],
+        description: '',
+        priceFrom: '',
+    },
+    outstanding = {
+        numbers: [9,10,11,12],
+        features: [],
+        description: '',
+        priceFrom: '',
+    },
+    superior = {
+        numbers: [12,14,15,16],
+        features: [],
+        description: '',
+        priceFrom: '',
     }
 ]
